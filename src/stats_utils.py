27b1c0171e900df820272164f4b329e6fdd3ce4a
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-06-15 16:52:31 +0200

    adding s3 upload function

--- src/stats_utils.py
+++ src/stats_utils.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import pandas as pd
+import boto3
 
 def calculate_stats_for_color(batch, color, key_prefix=''):
     """
@@ -49,3 +50,15 @@ def stats_to_parquet(batch_array, output_path) -> pd.DataFrame:
     print("Stats saved to {}".format(output_path))
 
     return df
+
+def stats_to_parquet_and_upload(batch_array, local_path: str, bucket_name: str, key: str):
+    """
+    """
+    # Generate Parquet file locally
+    df = stats_to_parquet(batch_array, local_path)
+
+    # Upload to S3
+    s3 = boto3.client('s3')
+    with open(local_path, 'rb') as f:
+        s3.upload_fileobj(f, bucket_name, key)
+    print(f"Uploaded to s3://{bucket_name}/{key}")
